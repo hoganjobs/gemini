df4cf501a680fa2fbc71efc34a16f4387280f549
王鸿桔 <hongju110@126.com>
2020-01-08 18:50:37 +0800

    init kvue

--- src/krouter/krouter-view.js
+++
@@ -1,8 +0,0 @@
-export default {
-  render(h) {
-    const {routeMap, current} = this.$router;
-    console.log(routeMap, current)
-    const component = routeMap[current].component || null;
-    return h(component)
-  }
-}
