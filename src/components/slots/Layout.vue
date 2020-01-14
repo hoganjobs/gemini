9fd25d985e520231a30b53b75b06e8376b6536e4
王鸿桔 <hongju110@126.com>
2020-01-14 10:40:07 +0800

    add slots

---
+++ src/components/slots/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .header {
+  background-color: rgb(252, 175, 175);
+}
+.body {
+  display: flex;
+  background-color: rgb(144, 250, 134);
+  min-height: 100px;
+  align-items: center;
+  justify-content: center;
+}
+.footer {
+  background-color: rgb(114, 116, 255);
+}
+</style>
---
+++ src/components/slots/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>slot</h3>
+    <Layout>
+      
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "@/component/slots/Layout.vue"
+  export default {
+    components: {
+      Layout
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
