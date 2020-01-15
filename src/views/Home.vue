f1f3c1ba9c8a7f83952edd2a805583c7649b3725
王鸿桔 <hongju110@126.com>
2020-01-15 17:52:14 +0800

    update img & home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <communication></communication> -->
     <slot-example></slot-example>
